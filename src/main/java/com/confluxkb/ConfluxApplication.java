1494fb316f13982c6a612b5772224ed052ca5f2f
bmlevchenko <bmlevchenko@gmail.com>
2015-12-13 22:01:08 +0200

    Initial application structure

---
+++ src/main/java/com/confluxkb/ConfluxApplication.java
@@ -0,0 +1,12 @@
+package com.confluxkb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfluxApplication.class, args);
+    }
+}
